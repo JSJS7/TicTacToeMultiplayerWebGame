38428594382cc9d54a57ba7621dbca731e2369d5
Jan Serwin <xd2137p@gmail.com>
2025-10-27 15:34:55 +0100

    skibidiv6

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
 const URL = import.meta.env.PROD
-  ? "https://your-render-app.onrender.com"
+  ? window.location.origin
   : "http://localhost:3001";
 
 export const socket = io(URL, {
-  autoConnect: false
+  autoConnect: false,
 });
