f4c3e284c335109fcb0c2d5aa9039631b06ae651
Jan Serwin <xd2137p@gmail.com>
2025-10-27 14:59:11 +0100

    skibidiv2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const httpServer = createServer(app);
